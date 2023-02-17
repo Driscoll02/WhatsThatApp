490686e4135f3c0c8f3c3c52f9e3d628b296e94d
Joseph Driscoll <driscoll2507@gmail.com>
2023-02-17 16:35:09 +0000

    Slightly changed styling and added title
    Only minor changes to styling. Added a title to login page

--- components/login.js
+++ components/login.js
@@ -59,14 +59,18 @@ class Login extends Component {
 
     render() {
         return (
-            <View style={styles.formContainer}>
-                <TextInput style={styles.fieldContainer} placeholder='Email' onChangeText={value=>this.setState({email:value})} />
-                <TextInput style={styles.fieldContainer} secureTextEntry={true} placeholder='Password' onChangeText={value=>this.setState({password:value})} />
-                <Pressable style={styles.loginButton} onPress={this._onButtonPress}>
-                    <Text>Login</Text>
-                </Pressable>
-                <View>
-                    <Text style={this.state.error == "Logging you in!" ? styles.noErrorMessage : styles.errorMessage}>{this.state.error}</Text>
+            <View style={styles.screenContainer}>
+                <Text style={styles.titleTextContainer}>WhatsThat?</Text>
+                <View style={styles.formContainer}>
+                <Text style={{marginBottom: 20, color: 'black', fontSize: '1.8em'}}>Enter your login:</Text>
+                    <TextInput style={styles.fieldContainer} placeholder='Email' onChangeText={value=>this.setState({email:value})} />
+                    <TextInput style={styles.fieldContainer} secureTextEntry={true} placeholder='Password' onChangeText={value=>this.setState({password:value})} />
+                    <Pressable style={styles.loginButton} onPress={this._onButtonPress}>
+                        <Text>Login</Text>
+                    </Pressable>
+                    <View>
+                        <Text style={this.state.error == "Logging you in!" ? styles.noErrorMessage : styles.errorMessage}>{this.state.error}</Text>
+                    </View>
                 </View>
             </View>
         )
@@ -74,28 +78,44 @@ class Login extends Component {
 }
 
 const styles = StyleSheet.create({
-    formContainer: {
-        backgroundColor: '#87FF8D',
-        width: '80%',
-        height: '50%',
-        borderRadius: '5%',
+    screenContainer: {
+        width: '100%',
+        height: '100%',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
     },
+    titleTextContainer: {
+        backgroundColor: '#149966', 
+        padding: 10, 
+        marginBottom: 30, 
+        borderRadius: 10, 
+        color: 'white', 
+        fontSize: '2.5em'
+    },
+    formContainer: {
+        backgroundColor: '#87FF8D',
+        width: '80%',
+        height: '50%',
+        borderRadius: 20,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
     fieldContainer: {
         marginTop: 20,
         marginBottom: 20,
-        border: 10,
-        borderColor: 'black',
+        padding: 10,
+        borderRadius: 10,
         color: 'black',
         backgroundColor: 'white'
     },
     loginButton: {
         width: '35%',
         textAlign: 'center',
+        margin: 5,
         padding: 10,
-        borderRadius: '15%',
+        borderRadius: 10,
         backgroundColor: 'white',
         fontWeight: 'bold'
     },
