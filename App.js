b41188313bf4c02264dfdd6bc95bba2cc98da07e
Joseph Driscoll <driscoll2507@gmail.com>
2023-02-18 12:09:50 +0000

    Signup page created. Copied from login page.
    Signup page is copied from login page

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './components/login';
+import SignUp from './components/signup';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Login />
+      {/*<Login />*/}
+      <SignUp />
     </View>
   );
 }
---
+++ components/signup.js
@@ -0,0 +1,159 @@
+import React, { Component } from 'react';
+import { Text, TextInput, View, Pressable, StyleSheet } from 'react-native';
+
+import * as EmailValidator from 'email-validator';
+
+class SignUp extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: "",
+            password: "",
+            submitted: false,
+            error: ""
+        }
+
+        this._onButtonPress = this._onButtonPress.bind(this);
+    }
+
+    _validateInputs() {
+        // Check if fields are empty
+        if(this.state.email === "" || this.state.password === "") {
+            this.setState({error:"Either the email or password field is empty!"});
+            this.setState({submitted:false})
+            return;
+        } 
+        
+        // Validate email
+        if(!EmailValidator.validate(this.state.email)) {
+            this.setState({error:"Email was incorrect"});
+            this.setState({submitted:false})
+            return;
+        }
+
+        // Validate password
+        const re = new RegExp("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$");
+        if(re.test(this.state.password) === false) {
+            this.setState({error:"Password was incorrect"});
+            this.setState({submitted:false})
+            return;
+        }
+
+        // If all validation passes
+        if(EmailValidator.validate(this.state.email) && re.test(this.state.password) === true) {
+            this.setState({error:"Logging you in!"});
+            return;
+        }
+
+        return;
+    }
+
+    _onButtonPress() {
+        // reset submitted and error state
+        this.setState({submitted:true});
+        this.setState({error:""});
+
+        // Validate form inputs
+        this._validateInputs();
+
+        return;
+    }
+
+    render() {
+        return (
+            <View style={styles.screenContainer}>
+                <Text style={styles.titleTextContainer}>WhatsThat?</Text>
+                <View style={styles.formContainer}>
+                    <View style={styles.formFieldsContainer}>
+                        <Text style={{marginBottom: 20, color: 'black', fontSize: '1.8em'}}>Enter your login:</Text>
+                        <TextInput style={styles.fieldContainer} placeholder='Email' onChangeText={value=>this.setState({email:value})} />
+                        <TextInput style={styles.fieldContainer} secureTextEntry={true} placeholder='Password' onChangeText={value=>this.setState({password:value})} />
+                        <Pressable style={styles.loginButton} onPress={this._onButtonPress}>
+                            <Text>Login</Text>
+                        </Pressable>
+                    </View>
+                    <View style={styles.formExtrasContainer}>
+                        <Text style={this.state.error == "Logging you in!" ? styles.noErrorMessage : styles.errorMessage}>{this.state.error}</Text>
+                        <Text style={styles.signUpText}>Don't have an account? Click here!</Text>
+                    </View>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    screenContainer: {
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    titleTextContainer: {
+        backgroundColor: '#149966', 
+        padding: 10, 
+        marginBottom: 30, 
+        borderRadius: 10, 
+        color: 'white', 
+        fontSize: '2.5em'
+    },
+    formContainer: {
+        backgroundColor: '#87FF8D',
+        width: '80%',
+        height: '50%',
+        borderRadius: 20,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'relative'
+    },
+    formFieldsContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 7
+    },
+    fieldContainer: {
+        marginTop: 20,
+        marginBottom: 20,
+        padding: 10,
+        borderRadius: 10,
+        color: 'black',
+        backgroundColor: 'white'
+    },
+    loginButton: {
+        width: '35%',
+        textAlign: 'center',
+        margin: 5,
+        padding: 10,
+        borderRadius: 10,
+        backgroundColor: 'white',
+        fontWeight: 'bold'
+    },
+    formExtrasContainer: {
+        flex: 3,
+        textAlign: 'center',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    errorMessage: {
+        color: 'red',
+        alignSelf: 'flex-start',
+        alignItems: 'center',
+        width: '100%'
+    },
+    noErrorMessage: {
+        color: 'blue',
+        alignSelf: 'flex-start',
+        textAlign: 'center',
+        width: '100%'
+    },
+    signUpText: {
+        justifyContent: 'flex-end',
+        marginBottom: 15
+    }
+})
+
+export default SignUp
